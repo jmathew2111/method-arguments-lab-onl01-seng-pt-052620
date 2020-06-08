e0917b70c63b0a9236891f828e3a973b9105a90a
Justin Mathew <jmathew2111@yahoo.com>
2020-06-08 14:14:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,6 @@
     #lib/introduction_with_language.rb
     
     def introduction_with_language(name, language)
-      puts "Hi, my name is #{name} and I am 
-      learning to program in #{language}."
+      puts "Hi, my name is #{name} and I am learning to program in #{language}."
     end
     
